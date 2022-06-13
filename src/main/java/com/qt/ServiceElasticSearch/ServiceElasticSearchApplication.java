bdea9e16a6dec5527e815b1a4e1f7ddb4d9f87c9
tranquoctuan2001 <tranquoctuan3112001@gmail.com>
2022-06-13 16:39:14 +0700

    initial project

---
+++ src/main/java/com/qt/ServiceElasticSearch/ServiceElasticSearchApplication.java
@@ -0,0 +1,13 @@
+package com.qt.ServiceElasticSearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceElasticSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceElasticSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qt/ServiceElasticSearch/ServiceElasticSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qt.ServiceElasticSearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceElasticSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
